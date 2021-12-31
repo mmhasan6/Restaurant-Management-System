b3dcf30d1d21caac0b5fec617417ade1c1e1f87a
Mahmudul Hasan <mmhasan.uiu@gmail.com>
2021-12-31 12:13:26 +0600

    User login & registration system using Laravel Jetstream, Added extra coulumn in user table 'usertype'.

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('home', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
