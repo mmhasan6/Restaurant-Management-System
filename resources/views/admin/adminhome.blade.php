27d4fdf2f2bca4c88b92857bbb846aac3c3cd264
Mahmudul Hasan <mmhasan.uiu@gmail.com>
2021-12-31 12:34:06 +0600

    Creating Admin Login System

---
+++ resources/views/admin/adminhome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Admin Home</title>
+</head>
+<body>
+  <x-app-layout>
+
+  </x-app-layout>
+  <h1>Admin Home Page</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::get('redirects', [HomeController::class, 'redirects']); //for multi Auth
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
